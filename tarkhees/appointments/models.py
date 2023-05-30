419c8bcc9f2225362e08792fc21c17d0a992fa77
harethhmoud <hareth.hmoud@gmail.com>
2023-05-30 17:50:32 -0400

    Added Appointment model with fields

--- tarkhees/appointments/models.py
+++ tarkhees/appointments/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.utils import timezone
+
+
+class Appointment(models.Model):
+    SCHEDULED = 'SC'
+    CANCELLED = 'CA'
+    COMPLETED = 'CO'
+    STATUS_CHOICES = [
+        (SCHEDULED, 'Scheduled'),
+        (CANCELLED, 'Cancelled'),
+        (COMPLETED, 'Completed'),
+    ]
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField('appointment date')
+    time = models.DateTimeField('appointment time')
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default=SCHEDULED,
+    )
+
+    def __str__(self):
+        return self.name + ", " + str(self.date) + ", " + str(self.time) + ", " + self.status
